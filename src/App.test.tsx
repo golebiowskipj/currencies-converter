9d1f5f177c42b67f9aa7f25a919e89cfcd744486
golebiowskipj <golebiowskipj@gmail.com>
2022-06-05 21:00:27 +0200

    test: write fake app tests

---
+++ src/App.test.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { App } from './App'
+
+describe('Tests placeholder', () => {
+  it.todo('write app tests ')
+})
