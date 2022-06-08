6734579f8213c9a592080ba043fb2b51e4b0a97a
Piotr G <42773035+golebiowskipj@users.noreply.github.com>
2022-06-08 19:00:14 +0200

    feat: converted to form field (#8)

--- src/conversion-widget/ConversionForm/ConversionForm.component.tsx
+++ src/conversion-widget/ConversionForm/ConversionForm.component.tsx
@@ -2,8 +2,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react/jsx-props-no-spreading */
 import { useTranslations } from 'translations'
-import { Button } from 'ui-kit'
+import { Button, InputNumber } from 'ui-kit'
 import { AmountField } from './fields/AmountField.component'
+import { ConvertedToField } from './fields/ConvertedToField.component'
 import { ConversionFormData, useConversionForm } from './useConversionForm'
 
 export const ConversionForm = () => {
@@ -17,6 +18,7 @@ export const ConversionForm = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <AmountField />
+      {formStatus === 'CONVERTED' && <ConvertedToField />}
       {formStatus === 'INITIAL' && (
         <Button type='submit'>
           {t('conversionWidget.conversionForm.submitButton')}
--- src/conversion-widget/ConversionForm/fields/ConvertedToField.component.tsx
+++ src/conversion-widget/ConversionForm/fields/ConvertedToField.component.tsx
@@ -2,32 +2,31 @@ import { Controller } from 'react-hook-form'
 import { useTranslations } from 'translations'
 import { InputNumber } from 'ui-kit'
 import { StyledErrorMessage } from '../ConversionForm.styles'
-import { FIELD_AMOUNT, useConversionForm } from '../useConversionForm'
+import { FIELD_CONVERTED_TO, useConversionForm } from '../useConversionForm'
 
-export const AmountField = () => {
-  const { control, errors, formStatus } = useConversionForm()
+export const ConvertedToField = () => {
+  const { control, errors } = useConversionForm()
   const t = useTranslations()
 
   return (
     <>
       <Controller
-        name={FIELD_AMOUNT}
+        name={FIELD_CONVERTED_TO}
         control={control}
         render={({ field: { value, onChange } }) => (
           <InputNumber
-            id={FIELD_AMOUNT}
+            id={FIELD_CONVERTED_TO}
             type='number'
-            label={t('conversionWidget.conversionForm.fieldAmount.label')}
+            label={t('conversionWidget.conversionForm.fieldConvertedTo.label')}
             endAdornment='EUR'
             value={value}
             onChange={onChange}
-            error={!!errors[FIELD_AMOUNT]?.message}
-            fullWidth={formStatus === 'INITIAL'}
+            error={!!errors[FIELD_CONVERTED_TO]?.message}
           />
         )}
       />
       <StyledErrorMessage>
-        {errors[FIELD_AMOUNT] && errors[FIELD_AMOUNT]?.message}
+        {errors[FIELD_CONVERTED_TO] && errors[FIELD_CONVERTED_TO]?.message}
       </StyledErrorMessage>
     </>
   )
